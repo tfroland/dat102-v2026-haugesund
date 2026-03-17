3c7a82b85ab163c4b74f150ca2e6116491bc918e
Tord Waage Frøland <tfroland@gmail.com>
2026-03-17 10:05:18 +0100

    Filer til F17

---
+++ f17-hashordbok-losning/src/dat102/f17/Main.java
@@ -0,0 +1,23 @@
+package dat102.f17;
+
+import java.util.Iterator;
+
+public class Main {
+
+   void main() {
+
+        // Testdata
+       String[] inventar = {"sverd", "lembasbrød", "pil", "sverd", "pil", "sverd"};
+
+        // Lag ordbok
+
+
+        // Tell opp
+
+
+        // Skriv frekvensoversikt
+
+
+    }
+
+}
---
+++ f17-hashordbok-losning/src/dat102/f17/Ordbok.java
@@ -0,0 +1,59 @@
+package dat102.f17;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Ordbok<K, V> implements OrdbokInterface<K, V> {
+
+    private HashMap<K, V> ordbok;
+
+    public Ordbok() {
+
+    }
+
+    @Override
+    public V leggTil(K nokkel, V verdi) {
+
+    }
+
+    @Override
+    public V fjern(K nokkel) {
+
+    }
+
+    @Override
+    public V getVerdi(K nokkel) {
+
+    }
+
+    @Override
+    public boolean inneholder(K nokkel) {
+
+    }
+
+    @Override
+    public Iterator<K> getNokkelIterator() {
+
+    }
+
+    @Override
+    public Iterator<V> getVerdiIterator() {
+
+    }
+
+    @Override
+    public boolean erTom() {
+
+    }
+
+    @Override
+    public int getAntall() {
+
+    }
+
+    @Override
+    public void toem() {
+
+    }
+
+}
