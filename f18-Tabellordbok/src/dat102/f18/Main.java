b6b6d2e907c2652fa6fd37209a7f7b1b32032908
Tord Waage Frøland <tfroland@gmail.com>
2026-03-20 15:05:40 +0100

    Oppdaterte filer for F18

--- f18-Tabellordbok/src/dat102/f18/Main.java
+++ f18-Tabellordbok/src/dat102/f18/Main.java
@@ -1,11 +1,38 @@
 package dat102.f18;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        TabellOrdbok<Integer, String> hm = new TabellOrdbok<>(100);
+        Integer tall = 15;
+        String s = new String("femten");
+        hm.leggTil(tall, s);
+
+        String gmlVerdi = hm.leggTil(tall, "Femten");
+        System.out.println(gmlVerdi);
+
+        System.out.println(hm.getVerdi(tall));
 
 
+        TabellOrdbok<String, ArrayList<String>> pokemonTrenere = new TabellOrdbok<>(100);
+        String trener1 = "Ash";
+        ArrayList<String> trener1pokemoner = new ArrayList<>();
+        trener1pokemoner.add("Pikachu");
+        trener1pokemoner.add("Pidgey");
+        trener1pokemoner.add("Krabby");
+
+        String trener2 = "Gary";
+        ArrayList<String> trener2pokemoner = new ArrayList<>((Arrays.asList("Rapidash", "Ninetales", "Charizard")));
+
+        pokemonTrenere.leggTil(trener1, trener1pokemoner);
+        pokemonTrenere.leggTil(trener2, trener2pokemoner);
+
+        System.out.println(pokemonTrenere.getVerdi(trener1));
+        System.out.println(pokemonTrenere.getVerdi(trener2));
     }
 
 }
